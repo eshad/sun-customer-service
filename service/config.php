5cb16e05a467aa14b327d2f76eb0dc52a68be9f5
Debian <uiu025@gmail.com>
2024-06-25 13:26:12 +0000

    updated commit 24Jun24

---
+++ service/config.php
@@ -0,0 +1,19 @@
+<?php
+// 这里填写客服系统域名，前面带http://，用于pusher系统通知客服平台客户或者客服上下线
+$domain = 'http://47.129.4.65';
+
+// App_key，客服系统与pusher通讯的key
+$app_key = 'sxzjm9jrrbilshbs';
+
+// App_secret，客服系统与pusher通讯的密钥
+$app_secret = 'xaghrfzwtzryilxt0ckwk73qvife30mk';
+
+// App id
+$app_id = 232;
+
+// websocket 端口，客服系统网页会连这个端口
+$websocket_port = 9090;
+
+// Api 端口，用于后端与pusher通讯
+$api_port = 2080;
+
