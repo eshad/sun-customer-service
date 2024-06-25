58b6a6b39d672be1593bf746ccdb078ce28bba92
Debian <uiu025@gmail.com>
2024-06-25 13:30:03 +0000

    index, database, runtime added

---
+++ public/index.php
@@ -0,0 +1,55 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+
+ini_set('session.gc_maxlifetime', 432000);
+ini_set('session.cookie_lifetime', 432000);
+ini_set('session.gc_probability',1);
+ini_set('session.gc_divisor',1000);
+
+isset($_SESSION) or session_start();
+
+// 定义环境版本
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+define('VENDOR',__DIR__.'/../vendor/');
+
+// 定义配置文件目录
+define('CONF_PATH', __DIR__ . '/../config/'); 
+ 
+
+// 定义pusher密匙
+define('app_key','sxzjm9jrrbilshbs');
+define('app_secret','xaghrfzwtzryilxt0ckwk73qvife30mk');
+define('app_id',232);
+define('whost','ws://kefu7hgshjagskgahsa4.online');
+define('ahost','http://kefu7hgshjagskgahsa4.online');
+define('wport',9090);
+define('aport',2080);
+define('registToken','1973438824');
+define('AIKF_SALT','xm3ycb1vl8h2rr2kct');
+define('AKF_VERSION','AI_KF');
+
+// 自定义一个 入口 目录
+define('PUBLIC_PATH',__DIR__);
+// 定义 类的文件路径
+define('EXTEND_PATH','../extend/');
+
+// 定义微信配置
+define('appid','');
+define('appsecret','');
+define('token','');
+define('domain','http://8.218.59.246');
+
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
---
+++ runtime/temp/e2263062eff96b4fb836cf563eefb2d3.php
@@ -0,0 +1,164 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:79:"/www/wwwroot/www.kefu.com/public/../application/service/view/index/service.html";i:1715055254;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>客服列表</title>
+    <script>
+        ROOT_URL = '<?php echo request()->root(); ?>';
+    </script>
+    <link rel="stylesheet" href="/assets/libs/layui/css/layui.css?v=AI_KF">
+    <script type="text/javascript" src="/assets/libs/jquery/jquery.min.js?v=AI_KF"></script>
+    <script src="/assets/libs/layer/layer.js?v=AI_KF" type="text/javascript"></script>
+    <style>
+        .layim-msgbox li {
+            padding: 0 24px;
+            height: 80px;
+        }
+
+        .layim-msgbox .layim-msgbox-tips {
+            margin: 0;
+            padding: 10px 0;
+            border: none;
+            text-align: center;
+            color: #999;
+        }
+
+        .layim-msgbox .layim-msgbox-system {
+            padding: 0 10px 10px 10px;
+        }
+
+        .layim-msgbox li p span {
+            padding-left: 5px;
+            color: #999;
+        }
+
+        .layim-msgbox li p em {
+            font-style: normal;
+            color: #FF5722;
+        }
+
+        .layim-msgbox-avatar {
+            margin-top: 17px;
+            height: 46px;
+            width: 46px;
+            float: left;
+            margin-right: 16px;
+        }
+
+        .layim-msgbox-user {
+            height: 80px;
+            line-height: 80px;
+            display: inline-block;
+            color: #555555;
+            width: 50%;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+
+        .layim-msgbox-content {
+            margin-top: 3px;
+        }
+
+        .layim-msgbox .layui-btn-small {
+            padding: 0 15px;
+            margin-left: 5px;
+        }
+
+        .layim-msgbox-btn {
+            float: right;
+            margin-top: 24px;
+            height: 32px;
+            line-height: 32px;
+            width: 66px;
+            border-radius: 16px;
+            background-color: #1E9FFF;
+            color: #fff;
+            text-align: center;
+            cursor: pointer;
+        }
+
+        .layim-msgbox-btn.layui-btn-disabled {
+            background-color: #dddddd;
+        }
+
+        .icon_gray {
+            -webkit-filter: grayscale(100%);
+            -ms-filter: grayscale(100%);
+            filter: grayscale(100%);
+            filter: gray;
+        }
+    </style>
+</head>
+<body>
+
+<ul class="layim-msgbox" id="LAY_view">
+
+    <?php if($service): foreach($service as $se): ?>
+      
+         <li>
+
+            <?php if($se['state'] == 'offline'): ?>
+            <a>
+                <img src="<?php echo $se['avatar']; ?>" class="layui-circle layim-msgbox-avatar icon_gray">
+            </a>
+             <span class="layim-msgbox-user">
+                <a><?php echo $se['nick_name']; ?></a>
+            </span>
+            <button class="layui-btn layim-msgbox-btn layui-btn-disabled" id='se<?php echo $se['service_id']; ?>'>转接</button>
+            <?php else: ?>
+            <a>
+                <img src="<?php echo $se['avatar']; ?>" class="layui-circle layim-msgbox-avatar">
+            </a>
+             <span class="layim-msgbox-user">
+                <a><?php echo $se['nick_name']; ?></a>
+            </span>
+            <button class="layui-btn layim-msgbox-btn" id='se<?php echo $se['service_id']; ?>' onclick='changese(<?php echo $se['service_id']; ?>)'>转接</button>
+            <?php endif; ?>
+           
+        </li>
+       <?php endforeach; else: ?>
+       <div class="layui-flow-more">
+            <li class="layim-msgbox-tips">暂无其他客服</li>
+        </div>
+
+    <?php endif; ?>
+
+</ul>
+
+<script src="/assets/libs/layui/layui.js?v=AI_KF"></script>
+<script>
+  
+    var changese = function(id){
+  
+       var initdata={
+          id:id,
+          visiter_id:'<?php echo $visiter_id; ?>',
+          name:'<?php echo $name; ?>'
+       };
+  
+       $.ajax({
+          url:ROOT_URL+'/service/index/getswitch',
+          type:'post',
+          data:initdata,
+          dataType:'json',
+          success:function(res){
+              if(res.code == 0){
+                  layer.msg('转接成功',{end:function(){
+                      $("#se"+id).addClass('layui-btn-disabled');
+                      parent.getchat();
+                      layer.close();
+                   }});
+  
+              }
+          }
+       });
+  
+       
+    }
+  
+  </script>
+</body>
+</html>
